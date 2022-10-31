065467ea0c0a0d11615e2aa419b01f1921cb892f
Rachel <racheljhumes@gmail.com>
2022-10-31 17:10:30 -0600

    commit tree 5ba038d05e8e0088e37899d8ea47a6335ab54bc2

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
 
 // Write your code here
+
+const num1 = 2; 
+const num2 = 31;
+const multiply = num1 * num2;
+
+
+const random = Math.floor(Math.random() * 1000) + 1;
+
+Number.isInteger(random);
+
+const num3 = 16;
+const num4 = 6; 
+const mod = num3 % num4;
+
+const max = Math.max(19, 20);
